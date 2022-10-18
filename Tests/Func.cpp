89c318776fa48768202878d009b3a8f12c76b3d6
Paul Mugume Okwija <okwijapaul@gmail.com>
2022-10-19 00:42:53 +0300

    Update Func.cpp

--- Tests/Func.cpp
+++ Tests/Func.cpp
@@ -1,14 +1,15 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "../main.cpp"
+#include "main.cpp"
 
-TEST_CASE( "2: Factorial of 0 is 15", "[multi-file:2]" ) {
-    REQUIRE( Factorial(0) == 1 );
+TEST_CASE( "1: 1 to the power of 0", "[multi-file:2]" ) {
+    REQUIRE( power(1,0) == 1 );
 }
 
-TEST_CASE( "2: Factorials of 1 and higher are computed", "[multi-file:2]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE( "2: Powers of integers", "[multi-file:2]" ) {
+    REQUIRE( power(1,8) == 1 );
+    REQUIRE( power(2,3) == 8 );
+    REQUIRE( power(3,3) == 27 );
+    REQUIRE( power(10,2) == 100);
 }
+
